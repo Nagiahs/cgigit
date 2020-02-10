871a7359784c455bc7553ac92b2677db2227769f
Nagiahs <manucrenn@gmail.com>
2020-02-10 11:35:11 +0100

    c'est ma v2

--- projetTestGit1/src/projetTestGit1/Test.java
+++ projetTestGit1/src/projetTestGit1/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test git v1");
+		System.out.println("test git v2");
 	}
 
 }
